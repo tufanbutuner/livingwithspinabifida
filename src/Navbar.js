a73c1e00ef5a781f84d477acf65557c0042881c3
Tufan Butuner <tbutu001@gold.ac.uk>
2021-02-13 18:31:49 +0000

    Created navbar

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+function Navbar(){
+    return (
+        <nav className="navbar">
+            <h1>Living with Spina Bifida</h1>
+                <div className="links">
+                <a href="/">Home</a>
+                <a href="/">About</a>
+                <a href="/">Register</a>
+                </div>
+        </nav>
+    );
+}
+
+export default Navbar;
