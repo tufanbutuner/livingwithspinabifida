3ba8372704367c31a34cf2e450afcaea1dbbf3c1
Tufan Butuner <tbutu001@gold.ac.uk>
2021-02-18 15:40:14 +0000

    1 - creating navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,12 @@
-function Navbar(){
+import { Link } from 'react-router-dom';
+
+function Navbar() {
     return (
         <nav className="navbar">
             <h1>Living with Spina Bifida</h1>
                 <div className="links">
-                <a href="/">Home</a>
-                <a href="/">About</a>
-                <a href="/">Register</a>
+                <Link to="/">Home</Link>
+                <Link to="/register">Register</Link>
                 </div>
         </nav>
     );
