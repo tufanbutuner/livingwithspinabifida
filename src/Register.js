453d9bdbe5fed923ef45f733734ca0734c33451f
Tufan Butuner <tbutu001@gold.ac.uk>
2021-02-20 18:43:45 +0000

    Removed previous register form

--- src/Register.js
+++ src/Register.js
@@ -1,17 +1,3 @@
-function Register() {
-    return (
-        <div className="register">
-            <h2>Register</h2>
-            <form>
-                <input type="text" placeholder="First name" required></input>
-                <input type="text" placeholder="Surname" required></input>
-                <input type="text" placeholder="Username" required></input>
-                <input type="email" placeholder="Email" required></input>
-                <input type="password" placeholder="Password" required></input>
-                <button type="submit" class="register-btn">Register</button>
-            </form>
-        </div>
-    );
 }
  
 export default Register;
