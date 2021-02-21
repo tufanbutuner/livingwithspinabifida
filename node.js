57674282b66da9a13d3237c8b877502b8853701b
Tufan Butuner <tbutu001@gold.ac.uk>
2021-02-21 18:57:23 +0000

    hashed password

--- node.js
+++ node.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors')
+const bcrypt = require('bcrypt');
 const bodyParser = require('body-parser');
 const dbController = require('./Controller/DatabaseController');
 
@@ -11,7 +12,8 @@ app.use(cors())
 app.post("/register", async (req, res) =>
 {
   var sql = "INSERT INTO user (firstName, lastName, username, password, email) VALUES (?, ?, ?, ?, ?)"
-  var parameters = [req.body.firstName, req.body.surname, req.body.username, req.body.password, req.body.email]
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
+  var parameters = [req.body.firstName, req.body.surname, req.body.username, hashedPassword, req.body.email]
   var results = await dbController.InsertUpdateQuery(sql, parameters);
   results == 1 ? res.status(200).send("Success!") : res.status(400).send("Something went wrong");
 })
