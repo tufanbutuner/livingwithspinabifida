42dbc9f5e65823b0281248aa9eb0471617b32df0
Tufan Butuner <tufanbutuner@gmail.com>
2021-04-17 17:24:11 +0100

    Removed routes from App.js and placed into Routes.js
    - Cleaned up App.js file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import Navbar from './Navbar';
-import Home from './Home';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Register from './Register';
-import Login from './Login';
-import Profile from './Profile';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
@@ -11,12 +7,7 @@ function App() {
       <div className="App">
         <Navbar />
         <div className="content">
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/register" component={Register} />
-            <Route path="/login" component={Login} />
-            <Route path="/profile" component={Profile} />
-          </Switch>
+          <Routes />
         </div>
       </div>
     </Router>
