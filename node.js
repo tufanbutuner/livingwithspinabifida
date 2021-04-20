be5a07d9b0c73cc44dfb9a2c68bd6fcd15b66c75
Tufan Butuner <tufanbutuner@gmail.com>
2021-04-20 17:16:32 +0100

    Updated node.js

--- node.js
+++ node.js
@@ -3,15 +3,21 @@ const session = require('express-session');
 const cors = require('cors')
 const bcrypt = require('bcrypt');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const dbController = require('./Controller/DatabaseController');
 
 const app = express();
+
+app.use(cors({
+  origin: "http://localhost:3000",
+  methods: ["GET", "POST"],
+  credentials: true
+}));
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
-app.use(cors())
 
-app.post("/register", async (req, res) =>
-{
+app.post("/register", async (req, res) => {
   var sql = "INSERT INTO user (firstName, lastName, username, password, email) VALUES (?, ?, ?, ?, ?)"
   const hashedPassword = await bcrypt.hash(req.body.password, 10);
   var parameters = [req.body.firstName, req.body.surname, req.body.username, hashedPassword, req.body.email]
@@ -19,12 +25,30 @@ app.post("/register", async (req, res) =>
   results == 1 ? res.status(200).send("Success!") : res.status(400).send("Something went wrong");
 })
 
-app.post("/login", async (req, res) =>
-{
+app.post("/login", async (req, res, next) => {
   var sql = "SELECT * FROM user WHERE username = ?"
   var parameters = [req.body.username, req.body.password];
   var results = await dbController.SelectQuery(sql, parameters);
   const match = await bcrypt.compare(req.body.password, results[0].password);
-  match ? res.status(200).send("Success!") : res.send("Something went wrong");
-})
+
+  if (match) {
+    // req.session.username = results;
+    // console.log(req.session.username);
+    res.send(results);
+  } else {
+    res.send("Something went wrong, please try again.");
+  }
+  // match ? res.status(200).send("Success!") : res.send("Something went wrong");
+  console.log(match);
+  // console.log(req.session.username);
+});
+
+// app.get('/', (req, res) => {
+//   // res.render('home', {message : 'Welcome, ' + req.session.username});
+//   res.status(200).send({message : 'Welcome, ' + req.session.username});
+// });
+
+// app.post("/profile", (req, res) => {
+//   res.render('home', {message : 'Welcome, ' + req.session.username});
+// })
 app.listen(5000);
