cee40ba9aa165dfa9637ae859afa6d2863579dbc
Tufan Butuner <tufanbutuner@gmail.com>
2021-04-22 14:35:00 +0100

    Updated routes with Dashboard and Landing pages

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -3,14 +3,17 @@ import Home from '../Home';
 import Register from '../Register';
 import Login from '../Login';
 import Profile from '../Profile';
+import Landing from '../Landing';
+import Dashboard from '../Dashboard';
 
 function Routes() {
     return (
         <Switch>
-            <Route exact path="/" component={Home} />
+            <Route exact path="/" component={Landing} />
             <Route path="/register" component={Register} />
             <Route path="/login" component={Login} />
             <Route path="/profile" component={Profile} />
+            <Route path="/dashboard" component={Dashboard} />
           </Switch>
     )
 }
