5086cf875df491588d1af80a455fed0c5318702c
Tufan Butuner <tufanbutuner@gmail.com>
2021-04-22 14:34:42 +0100

    Created dashboard for logged in users

---
+++ src/Dashboard.js
@@ -0,0 +1,9 @@
+function Dashboard() {
+    return (
+        <div className="dashboard">
+            <h2>Welcome back to the dashboard, User</h2>
+        </div>
+    );
+}
+
+export default Dashboard;
