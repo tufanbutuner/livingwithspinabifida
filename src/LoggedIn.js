6bcdf9c3f6b90b5cb482914d0aa0d77fa1096387
Tufan Butuner <tufanbutuner@gmail.com>
2021-04-22 17:10:05 +0100

    Created logout function

--- src/LoggedIn.js
+++ src/LoggedIn.js
@@ -1,7 +1,18 @@
+import { useHistory } from 'react-router-dom';
+
 function LoggedIn() {
+
+    const history = useHistory();
+
+    const logout = () => {
+        sessionStorage.removeItem('token');
+        history.push('/login');
+    }
+
     return (
         <div className="home">
             <h1>You are already logged in!</h1>
+            <button onClick={logout}>Log out</button>
         </div>
     );
 }
