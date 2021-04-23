04154ad57a2cccb72383dcbfd2b4c752ad391d91
Tufan Butuner <tufanbutuner@gmail.com>
2021-04-23 02:54:46 +0100

    Created Protected Routes
    Checking sessionStorage for a token, if true, the user is authenticated.
    If the sessionStorage is empty, there is no active session therefore the user is not authenticated

---
+++ src/routes/ProtectedRoute.js
@@ -0,0 +1,18 @@
+import { Route, Redirect } from 'react-router-dom';
+
+// Checking to see if user is authenticated by looking at sessionStorage, if there is a token, the user is authenticated
+// if the sessionStorage is empty, there is no active session therefore the user is not authenticated
+const isUserAuthenticated = () => {
+        return sessionStorage.getItem('token') !== null;
+    }
+
+const ProtectedRoute = ({ component: Component, ...rest }) => (
+    <Route {...rest} render={(props) => (
+      isUserAuthenticated() === true ? <Component {...props} /> : <Redirect to={{
+            pathname: '/login',
+            state: { from: props.location }
+          }} />
+    )} />
+  )
+
+  export default ProtectedRoute;
