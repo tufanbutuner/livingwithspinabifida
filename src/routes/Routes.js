fbce6247949a169cce6ba404d28b22b29c5f96f1
Tufan Butuner <tufanbutuner@gmail.com>
2021-04-23 16:42:43 +0100

    Conditionally rendering homepage
    If user is logged in, homepage is Dashboard, else it is Landing page

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,16 +1,16 @@
 import { Switch, Route } from "react-router-dom";
-import Home from '../Home';
 import Register from '../Register';
 import Login from '../Login';
 import Profile from '../Profile';
 import Landing from '../Landing';
 import Dashboard from '../Dashboard';
 import ProtectedRoute from './ProtectedRoute';
+import { isUserAuthenticated } from './ProtectedRoute';
 
 function Routes() {
     return (
         <Switch>
-            <Route exact path="/" component={Landing} />
+            <Route exact path="/" render={() => (isUserAuthenticated() === true ? <Dashboard /> : <Landing />)} />
             <Route path="/register" component={Register} />
             <Route path="/login" component={Login} />
             <ProtectedRoute path="/profile" component={Profile}/>
