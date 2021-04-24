acbb7667e9c093e4daf6de0ca85cd70c10827e97
Tufan Butuner <tufanbutuner@gmail.com>
2021-04-24 02:44:54 +0100

    conditionally rendering home page

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -6,16 +6,17 @@ import Landing from '../Landing';
 import Dashboard from '../Dashboard';
 import ProtectedRoute from './ProtectedRoute';
 import { isUserAuthenticated } from './ProtectedRoute';
+import LoggedIn from "../LoggedIn";
 
 function Routes() {
     return (
         <Switch>
             <Route exact path="/" render={() => (isUserAuthenticated() === true ? <Dashboard /> : <Landing />)} />
-            <Route path="/register" component={Register} />
+            <Route path="/register" render={() => (isUserAuthenticated() === true ? <LoggedIn /> : <Register />)} />
             <Route path="/login" component={Login} />
             <ProtectedRoute path="/profile" component={Profile}/>
             <ProtectedRoute path="/" component={Dashboard} />
-          </Switch>
+        </Switch>
     )
 }
 
