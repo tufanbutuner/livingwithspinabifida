1afd815b8cc8ce8914de000f160242e3131a9096
Tufan Butuner <tufanbutuner@gmail.com>
2021-04-27 14:06:47 +0100

    Updated isAuth function
    We can tell that a user is authenticated if we can get a username from the localStorage

--- src/routes/ProtectedRoute.js
+++ src/routes/ProtectedRoute.js
@@ -2,10 +2,11 @@ import { Route, Redirect } from 'react-router-dom';
 
 // Checking to see if user is authenticated by looking at sessionStorage, if there is a token, the user is authenticated
 // if the sessionStorage is empty, there is no active session therefore the user is not authenticated
-export const isUserAuthenticated = () => {
-        return sessionStorage.getItem('token') !== null;
-    }
+export const isUserAuthenticated = (...rest) => {
+        return localStorage.getItem('username') !== null;
+}
 
+// Explain this code
 const ProtectedRoute = ({ component: Component, ...rest }) => (
     <Route {...rest} render={(props) => (
       isUserAuthenticated() === true ? <Component {...props} /> : <Redirect to={{
