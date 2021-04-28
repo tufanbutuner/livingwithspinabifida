599cabdfd37075b9e4ea07aee844b3d379d3fe76
Tufan Butuner <tufanbutuner@gmail.com>
2021-04-28 05:08:01 +0100

    Updated Profile page UI
    - added basic styling to Profile page

--- src/Profile.js
+++ src/Profile.js
@@ -1,10 +1,19 @@
+import Card from 'react-bootstrap/Card'
 const axios = require('axios');
 
 function Profile() {
     const username = localStorage.getItem('username');
+    const firstName = localStorage.getItem('firstName');
+    const lastName = localStorage.getItem('lastName');
     return (
         <div className="profile">
-            <h2>Welcome back {username}</h2>
+            <Card id="profileCard">
+                <Card.Body>
+                    <Card.Title className="profileTitle">{firstName} {lastName}</Card.Title>
+                    <Card.Subtitle className="profileSubtitle">@{username}</Card.Subtitle>
+                    <Card.Text className="profileBio"> User bio goes here.</Card.Text>
+                </Card.Body>
+            </Card>
         </div>
     );
 }
