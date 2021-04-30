784aab4201b9dce0e99e5a7d3ad32cfd756c9520
Tufan Butuner <tufanbutuner@gmail.com>
2021-04-30 04:33:41 +0100

    Created a feed with list of posts

--- src/Feed.js
+++ src/Feed.js
@@ -1,47 +1,37 @@
-import Card from 'react-bootstrap/Card'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function Feed() {
+
+    // creating a state for to represent our posts list
+    const [list, setList] = useState([]);
+
+    // Every time we reload the page, we make an Axios get request which changes when info in the db changes
+    useEffect(() => {
+        Axios.get("http://localhost:5000/getPosts").then((data) => {
+            setList(data.data);
+        });
+    }, []);
+    // we add an empty array so the useEffect only makes the request once rather than infinitely
     
-    var username = localStorage.getItem('username')
+    var username = localStorage.getItem('username');
+    var firstName = localStorage.getItem('firstName');
 
     return (
-        
-        <div className="feed1">
-            <h2>Welcome back to the feed, {username}</h2>
 
-            <div className="postFeed">
-                <Card>
-                    <Card.Header>
-                        <div className="d-flex justify-content-between align-items-center">
-                            <div className="d-flex justify-content-between align-items-center">
-                                <div className="ml-2">
-                                    <div className="h5 m-0">@LeeCross</div>
-                                    <div className="h7 text-muted">Miracles Lee Cross</div>
-                                </div>
-                            </div>
-                        </div>
-                    </Card.Header>
-                    <div className="card-body">
-                        <div className="text-muted h7 mb-2"> <i class="fa fa-clock-o"></i>10 min ago</div>
-                        <a className="card-link" href="#">
-                            <h5 className="card-title">Lorem ipsum dolor sit amet, consectetur adip.</h5>
-                        </a>
-
-                        <p className="card-text">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo recusandae nulla rem eos ipsa praesentium esse magnam nemo dolor
-                            sequi fuga quia quaerat cum, obcaecati hic, molestias minima iste voluptates.
-                        </p>
-                    </div>
-                    <div className="card-footer">
-                        <a href="#" class="card-link"><i class="fa fa-gittip"></i> Like</a>
-                        <a href="#" class="card-link"><i class="fa fa-comment"></i> Comment</a>
-                        <a href="#" class="card-link"><i class="fa fa-mail-forward"></i> Share</a>
-                    </div>
-                </Card>
-            </div>
+        <div className="feed">
+            <h1> All posts</h1>
+            {list.map((posts) => {
+                return (<div className="post">
+                    <h1>{posts.userId}</h1>
+                    <h2>{posts.postTitle}</h2>
+                    <p>{posts.postContent}</p>
+                </div>
+                )
+            })}
+           
         </div>
-    );
+    )
 }
 
 export default Feed;
