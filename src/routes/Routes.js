72e1b415e09446eb056db6221d5656edbf342870
Tufan Butuner <tufanbutuner@gmail.com>
2021-04-30 02:47:05 +0100

    Added feed to routes

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -3,19 +3,19 @@ import Register from '../Register';
 import Login from '../Login';
 import Profile from '../Profile';
 import Landing from '../Landing';
-import Dashboard from '../Dashboard';
 import ProtectedRoute from './ProtectedRoute';
 import { isUserAuthenticated } from './ProtectedRoute';
 import LoggedIn from "../LoggedIn";
+import Feed from "../Feed";
 
 function Routes() {
     return (
         <Switch>
-            <Route exact path="/" render={() => (isUserAuthenticated() === true ? <Dashboard /> : <Landing />)} />
+            <Route exact path="/" render={() => (isUserAuthenticated() === true ? <Feed /> : <Landing />)} />
             <Route path="/register" render={() => (isUserAuthenticated() === true ? <LoggedIn /> : <Register />)} />
             <Route path="/login" component={Login} />
             <ProtectedRoute path="/profile" component={Profile}/>
-            <ProtectedRoute path="/" component={Dashboard} />
+            <ProtectedRoute path="/" component={Feed} />
         </Switch>
     )
 }
