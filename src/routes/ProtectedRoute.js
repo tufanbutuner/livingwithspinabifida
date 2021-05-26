cf6be11ffa1586679bef8463742f4b3f0e453754
Tufan Butuner <tufanbutuner@gmail.com>
2021-05-26 17:22:16 +0100

    Updated protectedroute

--- src/routes/ProtectedRoute.js
+++ src/routes/ProtectedRoute.js
@@ -1,19 +1,20 @@
 import { Route, Redirect } from 'react-router-dom';
 
-// Checking to see if user is authenticated by looking at localStorage, if there is a token, the user is authenticated
+// Checking to see if user is authenticated by looking at localStorage, if the username is there, the user is authenticated
 // if the localStorage is empty, there is no active session therefore the user is not authenticated
 export const isUserAuthenticated = (...rest) => {
         return localStorage.getItem('username') !== null;
 }
 
-// Explain this code
+// Creating a protected route component where the props will be the components we want to protect
+// if the user is not authenticated then it will redirect them to the login page
 const ProtectedRoute = ({ component: Component, ...rest }) => (
     <Route {...rest} render={(props) => (
       isUserAuthenticated() === true ? <Component {...props} /> : <Redirect to={{
             pathname: '/login',
             state: { from: props.location }
           }} />
-    )} />
-  )
+  )} />
+)
 
-  export default ProtectedRoute;
+export default ProtectedRoute;
