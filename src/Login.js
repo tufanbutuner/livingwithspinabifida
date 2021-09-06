8b4dc491fd0b28f12687da2d0a7bf7ef3f1a82dd
tufanbutuner <tufanbutuner@gmail.com>
2021-09-06 19:39:27 +0100

    Front-end changes

--- src/Login.js
+++ src/Login.js
@@ -1,47 +1,58 @@
-import { useState } from 'react';
-import { useHistory } from 'react-router';
-import LoggedIn from './LoggedIn';
-import axios from 'axios';
+import LoggedIn from "./LoggedIn";
+import axios from "axios";
+import { useState } from "react";
 
 const Login = () => {
-    const history = useHistory();
-    const [user, setUser] = useState({});
-    const username = localStorage.getItem('username');
+  const [user, setUser] = useState({});
+  const username = localStorage.getItem("username");
 
-    const changeHandler = (e) => {
-        setUser({ ...user, [e.target.name]: e.target.value})
-    }
+  const existingUser = {
+    username: "",
+    password: "",
+  };
 
-    const submitHandler = async e => {
-        e.preventDefault();
-        // sending an axios post to node /login endpoint
-        var result = await axios.post("/login", user);
-        var data = result.data;
-        
-        if (data.password != undefined ) {
-            localStorage.setItem('userId', data.userId);
-            localStorage.setItem('username', data.username);
-            localStorage.setItem('name', data.name);
-            history.push('/profile');
-        } else {
-            alert('Incorrect username or password. Please try again');
-        }
-    }
-    
-    return ( username ? <LoggedIn /> :
-        <div className="login">
-            <h2>Login</h2>
-            <form onSubmit={submitHandler}>
-                <div>
-                    <input type="text" name="username" placeholder="Username" onChange={changeHandler} required></input>
-                </div>
-                <div>
-                    <input type="password" name="password" placeholder="Password" onChange={changeHandler} required></input>
-                </div>
-                <button type="submit" className="login-btn" onClick={submitHandler}>Login</button>
-            </form>
+  const changeHandler = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    // // sending an axios post to node /login endpoint
+    // var result = await axios.post("/login", user);
+    // var data = result.data;
+    axios.post("/login", user).then((res) => console.log(res.data));
+  };
+
+  return (
+    <div className="login">
+      <h2>Login</h2>
+      <form onSubmit={submitHandler}>
+        <div>
+          <input
+            type="text"
+            id="username"
+            placeholder="Username"
+            onChange={changeHandler}
+            value={user.username}
+            required
+          ></input>
         </div>
-    );
-}
+        <div>
+          <input
+            type="password"
+            id="password"
+            placeholder="Password"
+            onChange={changeHandler}
+            value={user.password}
+            required
+          ></input>
+        </div>
+        <button type="submit" className="login-btn" onClick={submitHandler}>
+          Login
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default Login;
--- src/Register.js
+++ src/Register.js
@@ -1,45 +1,88 @@
-import { useState } from 'react';
-import { useHistory } from 'react-router';
-const axios = require('axios');
+import { useState } from "react";
+const axios = require("axios");
 
 const Register = () => {
-    const history = useHistory();
-    const [user, setUser] = useState({});
+  const [user, setUser] = useState({});
 
-    const changeHandler = (e) => {
-        setUser({ ...user, [e.target.name]: e.target.value})
-    }
+  const newUser = {
+    name: user.name,
+    username: user.username,
+    email: user.email,
+    password: user.password,
+    password2: user.password2,
+  };
 
-    const submitHandler = async e => {
-        e.preventDefault();
-        var result = await axios.post("/register", user);
-        console.log(result);
-        history.push('/registered');
-    }
+  const changeHandler = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
 
-    return (
-        <div className="register">
-            <h2>Register</h2>
-            <form onSubmit={submitHandler}>
-                <div>
-                    <input type="text" placeholder="First name" name="firstName" onChange={changeHandler} required></input>
-                </div>
-                <div>
-                    <input type="text" placeholder="Surname" name="surname" onChange={changeHandler} required></input>
-                </div>
-                <div>
-                    <input type="text" name="username" placeholder="Username" onChange={changeHandler} required></input>
-                </div>
-                <div>
-                    <input type="email" name="email" placeholder="Email" onChange={changeHandler} required></input>
-                </div>
-                <div>
-                    <input type="password" name="password" placeholder="Password" onChange={changeHandler} required></input>
-                </div>
-                <button type="submit" className="register-btn" onClick={submitHandler}>Register</button>
-            </form>
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    axios.post("/register", user).then((res) => console.log(res.data));
+    console.log(user);
+  };
+
+  return (
+    <div className="register">
+      <h2>Register</h2>
+      <form onSubmit={submitHandler}>
+        <div>
+          <input
+            type="text"
+            placeholder="Name"
+            id="name"
+            onChange={changeHandler}
+            value={user.name}
+            required
+          ></input>
         </div>
-    )
-}
+        <div>
+          <input
+            type="text"
+            placeholder="Username"
+            id="username"
+            onChange={changeHandler}
+            value={user.username}
+            required
+          ></input>
+        </div>
+        <div>
+          <input
+            type="email"
+            id="email"
+            placeholder="Email"
+            onChange={changeHandler}
+            value={user.email}
+            required
+          ></input>
+        </div>
+        <div>
+          <input
+            type="password"
+            id="password"
+            placeholder="Password"
+            onChange={changeHandler}
+            value={user.password}
+            required
+          ></input>
+        </div>
+        <div>
+          <input
+            type="password"
+            id="password2"
+            placeholder="Re-type Password"
+            onChange={changeHandler}
+            value={user.password2}
+            required
+          ></input>
+        </div>
+        <button type="submit" className="register-btn" onClick={submitHandler}>
+          Register
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default Register;
